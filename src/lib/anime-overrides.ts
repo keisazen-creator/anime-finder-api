7ff44f3e29dedf17185cfdcb185de77c4251fc24
keisazen-creator <keisazen@gmail.com>
2026-04-10 17:00:35 +0530

    Add episode count overrides for anime

---
+++ src/lib/anime-overrides.ts
@@ -0,0 +1,10 @@
+// Episode count overrides for specific anime
+
+const episodeOverrides = {
+    "One Piece": {
+        episodeCount: 1054 // Updated episode count as of 2026-04-10
+    },
+    // Add other anime overrides here
+};
+
+export default episodeOverrides;
